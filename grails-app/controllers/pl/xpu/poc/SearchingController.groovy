e6fa0fb28ef980ba142e839028720f22eedb6a36
landydan <landydan@gmail.com>
2023-10-01 16:04:24 -0700

    error shows

--- grails-app/controllers/pl/xpu/poc/SearchingController.groovy
+++ grails-app/controllers/pl/xpu/poc/SearchingController.groovy
@@ -1,15 +1,24 @@
 package pl.xpu.poc
 
+import pl.xpu.SavedSearch
 
 class SearchingController {
 
     def index() {
-
+        render("temp")
     }
 
     def test(String search) {
         log.info("search: ${search}")
         render("testing ${search}")
     }
+
+    def something() {
+        Long searchId = params.id as Long
+        // below is the line that needs to be present
+        SavedSearch search = SavedSearch.read(searchId)
+        render("placeholder")
+    }
+
 
 }
